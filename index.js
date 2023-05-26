746cd32c9ebab16b5989f4001d2d813006acb7bd
Jim Taulman <wiredco@gmail.com>
2023-05-26 08:29:32 -0400

    fix axios request

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const axios = require('axios');
 
 async function TestRequest() {
-    await axios.get('http://date.jsontest.com/?service=date')
-        .then(res => {
-            console.log(res.data.url);
-            console.log(res.data.explanation);
-            return(res);
-        })
-        .catch(err => {
-            console.log("Error: " + err);
-        });
+    let response = await axios.get('http://date.jsontest.com/?service=date');
+    let statusCode = response.status;
+    let date = response.data.date;
+    let time = response.data.time;
+    
+    console.log(`Status: ${statusCode},\nDate: ${date},\nTime: ${time}`);
 };
 
 module.exports = { TestRequest };
